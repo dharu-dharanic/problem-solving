8d8e70e5a906762617c7ccfdbe8f0ecdcd48ec2f
dharu-dharanic <dharanic5651@gmail.com>
2026-05-06 09:13:30 +0530

    add: LC 20 Valid Parentheses - stack approach

--- LeetCode/Stack/20_ValidParentheses.java
+++ LeetCode/Stack/20_ValidParentheses.java
@@ -1,17 +1,25 @@
-// Leetcode 20. Valid Parentheses
-// https://leetcode.com/problems/valid-parentheses/
+/*
+Leetcode 20. Valid Parentheses
+https://leetcode.com/problems/valid-parentheses/
+Time Complexity: O(n)
+Space Complexity: O(n)
+*/
 
 package LeetCode.Stack;
-import java.util.Stack;
+
+import java.util.Deque;
+import java.util.ArrayDeque;
+
 class Solution {
     public boolean isValid(String s) {
-         Stack<Character> stack = new Stack<>();
+        Deque<Character> stack = new ArrayDeque<>();
 
         for (char ch : s.toCharArray()) {
             if (ch == '(' || ch == '{' || ch == '[') {
                 stack.push(ch);
             } else {
                 if (stack.isEmpty()) return false;
+
                 char top = stack.pop();
                 if ((ch == ')' && top != '(') ||
                     (ch == '}' && top != '{') ||
