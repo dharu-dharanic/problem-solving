897e0d62d98b1e9a853edf41660991609255a819
dharu-dharanic <dharanic5651@gmail.com>
2026-05-12 20:01:03 +0530

    add: LC 33 Search in Rotated Sorted Array

---
+++ LeetCode/BinarySearch/33_SearchRotatedSortedArray.java
@@ -0,0 +1,47 @@
+package LeetCode.BinarySearch;
+
+/*
+Leetcode 33. Search in Rotated Sorted Array
+https://leetcode.com/problems/search-in-rotated-sorted-array/
+
+Time Complexity: O(log n)
+Space Complexity: O(1)
+*/
+
+class Solution {
+
+    public int search(int[] nums, int target) {
+
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            
+            if (nums[left] <= nums[mid]) {
+                if (nums[left] <= target && target < nums[mid]) {
+                    right = mid - 1;
+
+                } else {
+                    left = mid + 1;
+                }
+
+            }
+
+            else {
+                if (nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1;
+
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+}
