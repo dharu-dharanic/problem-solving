c3628b773658a7c98084328469121624c097c04c
dharu-dharanic <dharanic5651@gmail.com>
2026-05-13 23:49:32 +0530

    add: LC 42 trapping rain water - Two pointer Array

---
+++ LeetCode/TwoPointer/42_TrappingRainWatet.java
@@ -0,0 +1,47 @@
+package LeetCode.TwoPointer;
+
+/*
+Leetcode 42 Trapping Rain water
+https://leetcode.com/problems/trapping-rain-water/
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+
+class Solution {
+    public int trap(int[] height) {
+
+        int left = 0;
+        int right = height.length - 1;
+
+        int leftMax = 0;
+        int rightMax = 0;
+
+        int water = 0;
+
+        while (left < right) {
+
+            if (height[left] < height[right]) {
+
+                if (height[left] >= leftMax) {
+                    leftMax = height[left];
+                } else {
+                    water += leftMax - height[left];
+                }
+
+                left++;
+
+            } else {
+
+                if (height[right] >= rightMax) {
+                    rightMax = height[right];
+                } else {
+                    water += rightMax - height[right];
+                }
+
+                right--;
+            }
+        }
+
+        return water;
+    }
+}
