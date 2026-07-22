f021f23f8d03430735821bb834290b70ffced404
dharu-dharanic <dharanic5651@gmail.com>
2026-07-23 01:16:12 +0530

    add: LC 21 Merge Two Sorted Lists - Linked List approach

---
+++ LeetCode/LinkedList/21_MergeTwoSortedLists.java
@@ -0,0 +1,33 @@
+/*
+Leetcode 21. Merge Two Sorted Lists
+https://leetcode.com/problems/merge-two-sorted-lists/
+
+Time Complexity: O(n + m)
+Space Complexity: O(1)
+*/
+
+class Solution {
+
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+
+        while (list1 != null && list2 != null) {
+
+            if (list1.val <= list2.val) {
+                current.next = list1;
+                list1 = list1.next;
+            } else {
+                current.next = list2;
+                list2 = list2.next;
+            }
+
+            current = current.next;
+        }
+
+        current.next = (list1 != null) ? list1 : list2;
+
+        return dummy.next;
+    }
+}
