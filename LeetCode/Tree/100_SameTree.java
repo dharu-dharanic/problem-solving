5577643e3f12e37fcee070f7f7d8142209501ec8
dharu-dharanic <dharanic5651@gmail.com>
2026-07-29 23:49:22 +0530

    add: LC 100 Same Tree  - Tree Implementation

---
+++ LeetCode/Tree/100_SameTree.java
@@ -0,0 +1,30 @@
+package LeetCode.Tree;
+
+/*
+Leetcode 100. Same Tree
+https://leetcode.com/problems/same-tree/
+
+Time Complexity: O(n)
+Space Complexity: O(h)
+*/
+
+class Solution {
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+
+        if (p == null && q == null) {
+            return true;
+        }
+
+        if (p == null || q == null) {
+            return false;
+        }
+
+        if (p.val != q.val) {
+            return false;
+        }
+
+        return isSameTree(p.left, q.left)
+                && isSameTree(p.right, q.right);
+    }
+}
