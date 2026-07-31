96607d1eeff21276f4bb933f2aa4b145f743bba7
Dharani <dharanic5651@gmail.com>
2026-07-31 09:10:14 +0530

    Time: 0 ms (100.00%) | Memory: 47.4 MB (38.29%) - LeetSync

---
+++ LeetCode/Arrays/566-reshape-the-matrix/reshape-the-matrix.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[][] matrixReshape(int[][] mat, int r, int c) {
+        int m = mat.length;
+        int n = mat[0].length;
+
+        // Check if reshape is possible
+        if (m * n != r * c) {
+            return mat;
+        }
+
+        int[][] res = new int[r][c];
+        int idx = 0;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                res[idx / c][idx % c] = mat[i][j];
+                idx++;
+            }
+        }
+
+        return res;
+    }
+}
