e640f053765b864a60653ded1ae2acfdaa400f46
dharu-dharanic <dharanic5651@gmail.com>
2026-07-31 14:15:21 +0530

    add: LC 24 Swap Nodes in Pairs - iterative linked list

---
+++ LeetCode/LinkedList/24-swap-nodes-in-pairs.java
@@ -0,0 +1,34 @@
+package LeetCode.LinkedList;
+
+/*
+Leetcode 24. Swap Nodes in Pairs
+https://leetcode.com/problems/swap-nodes-in-pairs/
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+
+class Solution {
+
+    public ListNode swapPairs(ListNode head) {
+
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode prev = dummy;
+
+        while (prev.next != null && prev.next.next != null) {
+
+            ListNode first = prev.next;
+            ListNode second = first.next;
+
+            first.next = second.next;
+            second.next = first;
+            prev.next = second;
+
+            prev = first;
+        }
+
+        return dummy.next;
+    }
+}
